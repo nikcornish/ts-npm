6fdb0825bff3e2a028233b849f132a1ddbaa1afd
Nicholas Cornish <Nicholas.Cornish@de.bosch.com>
2021-07-04 12:26:25 +0200

    folder tidyup

---
+++ src/basics/any.ts
@@ -0,0 +1,13 @@
+// Type any allows any type, but leaves the door open for errors, isn't strict enough
+var b : any;
+b = true
+b = false
+b = 10
+b = undefined
+
+
+// Union Type
+var c : number | string
+c = 10
+c = 'yeah!'
+c = false // not allowed, only number or string
